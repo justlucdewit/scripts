ea03189fa212974607f6043b0a25cb57a91b2a44
luc.dewit <luc.dewit@hotmail.nl>
2024-10-21 20:08:10 +0200

    Added ccvm to projects

--- inject/definitions.sh
+++ inject/definitions.sh
@@ -1,15 +1,19 @@
 unset -v projects
-declare -A projects=(
+declare -gA projects=(
+    # Work projects
     ["scripts"]="$HOME/./scripts"
     ["tta"]="$HOME/projects/tta-onepager"
     ["xin-front"]="$HOME/projects/x-ingredient-company-website-new"
     ["xin-back"]="$HOME/projects/x-ingredient-api-v2"
     ["spectrum"]="$HOME/projects/website-spectrum-59-q3-2022"
     ["clean-laravel-proj"]="$HOME/projects/clean-laravel-project"
+    
+    # Private projects
+    ["CCVM"]="$HOME/projects/ccvm"
 )
 
 unset -v user_map
-declare -A user_map=(
+declare -gA user_map=(
     ["CK"]="Cem"
     ["Luc de Wit"]="Luc"
     ["Reinout Boelens"]="Reinout"
