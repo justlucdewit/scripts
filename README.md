c29c4dfeb8828ed0b74dce6f3414756d11a4d36a
Luc de Wit <luc.dewit@hotmail.nl>
2024-10-21 20:46:55 +0200

    Updated readme

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A repository containing handy bash scripts to make (development) work in Linux U
 
 It is easy to install, and it is easy to update automatically without having to worry.
 
-# Installation guide
+## Installation guide
 To install Lukes script repository. you need to run 2 simple commands.
 
 ```bash
@@ -16,3 +16,36 @@ git clone https://github.com/justlucdewit/scripts ~/scripts
 
 This should succesfully install Lukes Script Repository. No restart is needed.
 Everything should be emediately available
+
+## Usage
+The following is a list of commands to be used in LSR
+
+### **Simple Standalone Commands**
+
+| command | usage | description |
+|---------|-------|-------------|
+| c | c | clear the terminal |
+| l | l | display a vertical list of files/folders in the current working directory |
+| p | p <projectname> | jump to the location of a project |
+| rb | rb | reload bash (sources the bashrc file) |
+
+
+### **Tmux commands**
+| command | usage | description |
+|---------|-------|-------------|
+| t | t | open tmux in the current dir |
+| shor | shor | Split the current pane horizontally |
+| sver | sver | Split the current pane vertically |
+| ml | ml | move to the pane on the left |
+| mr | mr | move to the pane on the right |
+| mu | mu | move to the pane that is upwards |
+| md | md | move to the pane that is downward |
+| el | el <n=5> | expand pane n columns to the left |
+| er | er <n=5> | expand pane n columns to the right |
+| eu | eu <n=5> | expand pane n columns up |
+| ed | ed <n=5> | expand pane n columns down |
+| tls | tls | lists all of the panes in all of the windows in all of the sessions |
+| rp | rp <name> | rename the pane to the given name |
+| tc | tc | close the current pane |
+| tca | tca | close all panes |
+| rip | rip <pane num> <command> | runs a command in the given pane |
--- inject/tmux_helpers.sh
+++ inject/tmux_helpers.sh
@@ -1,4 +1,3 @@
-# TODO: Fix bug where using 2 sessions in paralel destroys tmux_pane_names.txt in localdata
 # TODO: Fix bug where sometimes pane names are forgotten
 
 INDENT="    "
@@ -204,13 +203,22 @@ tlist() {
     load_pane_names
     sync_pane_names
 
+    # Get the active session, window, and pane
+    active_session=$(tmux display-message -p '#S')
+    active_window=$(tmux display-message -p '#I')
+    active_pane=$(tmux display-message -p '#P')
+
     # List all tmux sessions
     tmux list-sessions -F '#S' | nl -w1 -s': ' | while read session; do
         session_number=$(echo "$session" | cut -d':' -f1 | xargs)  # Get the session number
         session_name=$(echo "$session" | cut -d':' -f2 | xargs)    # Get the session name
 
-        # Output session name with color
-        echo -e "${INACTIVE_LIST_ITEM}\e[1;34mSession $session_number: $session_name\e[0m"
+        # Check if session is active
+        if [ "$session_name" == "$active_session" ]; then
+            echo -e "${ACTIVE_LIST_ITEM}\e[1;34mSession $session_number: $session_name\e[0m"
+        else
+            echo -e "${INACTIVE_LIST_ITEM}\e[1;34mSession $session_number: $session_name\e[0m"
+        fi
 
         # List windows in the current session
         tmux list-windows -t "$session_name" -F '#I: #W' | while read -r window; do
@@ -218,9 +226,14 @@ tlist() {
             window_number=$(echo "$window" | cut -d':' -f1 | xargs)  # Get the window number
             window_name=$(echo "$window" | cut -d':' -f2 | xargs)    # Get the window name
 
-            # Output window number and name with color
-            echo -e "${INDENT}${INACTIVE_LIST_ITEM}\e[1;32mWindow $window_number: $window_name\e[0m"
+            # Check if window is active
+            if [ "$session_name" == "$active_session" ] && [ "$window_number" == "$active_window" ]; then
+                echo -e "${INDENT}${ACTIVE_LIST_ITEM}\e[1;32mWindow $window_number: $window_name\e[0m"
+            else
+                echo -e "${INDENT}${INACTIVE_LIST_ITEM}\e[1;32mWindow $window_number: $window_name\e[0m"
+            fi
 
+            # List panes in the current window
             tmux list-panes -t "$session_name:$window_number" -F '#P: #T' | while read -r pane; do
                 pane_number=$(echo "$pane" | cut -d':' -f1 | xargs)  # Get the pane number
 
@@ -228,8 +241,12 @@ tlist() {
                 pane_key="${session_name}:${window_number}:${pane_number}"
                 pane_name=${pane_names[$pane_key]:-"Unnamed Pane"}
 
-                # Output pane number and name with color
-                echo -e "${INDENT}${INDENT}${INACTIVE_LIST_ITEM}\e[1;33mPane $pane_number: $pane_name\e[0m"
+                # Check if pane is active
+                if [ "$session_name" == "$active_session" ] && [ "$window_number" == "$active_window" ] && [ "$pane_number" == "$active_pane" ]; then
+                    echo -e "${INDENT}${INDENT}${ACTIVE_LIST_ITEM}\e[1;33mPane $pane_number: $pane_name\e[0m"
+                else
+                    echo -e "${INDENT}${INDENT}${INACTIVE_LIST_ITEM}\e[1;33mPane $pane_number: $pane_name\e[0m"
+                fi
             done
         done    
     done
@@ -286,146 +303,3 @@ tcloseall() {
 alias tca="tcloseall"
 alias rip="run_in_pane"
 alias tc="tclose"
-
-# load_pane_names
-
-# set_window_name() {
-#     local new_name="$1"
-
-#     # Ensure the window is unique and not a race condition
-#     tmux rename-window "$new_name"
-# }
-
-# run_command_in_pane() {
-#     local pane_name="$1"
-#     local command="$2"
-
-#     # Check if the pane exists (using the current window)
-#     local current_window=$(tmux display-message -p '#W')
-
-#     # Create a new pane if it does not exist
-#     if ! tmux list-panes -t "$current_window" | grep -q "$pane_name"; then
-#         tmux split-window -h  # Split horizontally to create a new pane
-#         set_pane_name "$pane_name"  # Set the name for the new pane
-#     fi
-
-#     # Send the command to the specified pane
-#     tmux send-keys -t "$current_window:$pane_name" "$command" C-m  # C-m simulates pressing Enter
-# }
-
-# set_pane_name() {
-#     local new_name="$1"
-
-#     # Get the current pane ID
-#     local current_pane_id=$(tmux display-message -p '#P')
-
-#     # Rename the current pane using the pane ID
-#     tmux select-pane -t "$current_pane_id"
-#     tmux select-pane -T "$new_name"  # This sets the pane title
-# }
-
-# restore() {
-#     # Get the current pane ID (the one where the command was executed)
-#     local current_pane_id=$(tmux display-message -p '#P')
-
-#     # Get the list of all pane IDs, including their names for debugging
-#     local pane_ids=$(tmux list-panes -F '#P')
-
-#     for pane_id in $pane_ids; do
-#         if [[ "$pane_id" != "$current_pane_id" ]]; then
-#             echo "Sending stop signal to pane $pane_id"
-#             # Send the "SIGINT" signal (Ctrl+C) to the process running in the pane
-#             tmux send-keys -t "$pane_id" C-c  # Sends Ctrl+C to the pane
-#             tmux send-keys -t "$pane_id" "clear" C-m  # Sends Ctrl+C to the pane
-#             sleep 1  # Wait for the process to handle the signal
-#         fi
-#     done
-
-#     # Loop through the panes and kill each one that isn't the current pane
-#     for pane_id in $pane_ids; do
-#         if [[ "$pane_id" != "$current_pane_id" ]]; then
-#             # Attempt to close the pane
-#             if tmux kill-pane -t "$pane_id"; then
-#                 echo "Closed pane $pane_id"
-#             else
-#                 echo "Failed to close pane $pane_id"
-#             fi
-#         fi
-#     done
-# }
-
-
-# fresh() {
-#     sudo -rf node_modules
-#     sudo -rf vendor
-#     npm i
-#     composer i
-# }
-
-# init_session_name() {
-#     # Get the current session name
-#     local session_name=$(tmux display-message -p '#S')
-
-#     # Check if the session name is empty or just a number (unnamed)
-#     if [[ -z "$session_name" || "$session_name" =~ ^[0-9]+$ ]]; then
-#         # Generate a random name
-#         local random_name="session_$(date +%s%N | sha256sum | base64 | head -c 8)"
-        
-#         # Rename the current session
-#         tmux rename-session "$random_name"
-#         echo "Initialized session name to: $random_name"
-#     else
-#         echo "Current session name: $session_name"
-#     fi
-# }
-
-# list_panes() {
-#     # Get the current window ID
-#     local current_window=$(tmux display-message -p '#W')
-
-#     # List all panes in the current window with their IDs and titles
-#     echo "Panes in window '$current_window':"
-#     tmux list-panes -t "$current_window" -F "#{pane_id}: (#{pane_title})" | nl
-# }
-
-# start() {
-#     init_session_name
-#     declare -A npm_start_commands=(
-#         ["website-spectrum-59-q3-2022"]="dev"
-#     );
-
-#     declare -A sdfsdfsdf=(
-#     );
-
-#     local curr_dir=$(basename $(pwd))
-#     local current_window_index=$(tmux display-message -p '#I')
-#     local current_session=$(tmux display-message -p '#S')
-    
-#     if [[ -v npm_start_commands["$curr_dir"] ]]; then
-#         local curr_npm_start_command="${npm_start_commands[$curr_dir]}"
-
-#         local npm_pane="Npm-$curr_dir"
-#         local sail_pane="Sail-$curr_dir"
-#         local terminal_pane="Terminal-$curr_dir"
-        
-
-#         set_pane_name $sail_pane
-#         local sail_pane_index=$(tmux display-message -p '#P')
-#         shor
-#         set_pane_name $terminal_pane
-#         local terminal_pane_index=$(tmux display-message -p '#P')
-#         ml
-#         sver
-#         set_pane_name $npm_pane
-#         local npm_pane_index=$(tmux display-message -p '#P')
-#         mr
-
-#         tmux send-keys -t $current_session:$current_window.$npm_pane_index "npm run $curr_npm_start_command" C-m
-#         tmux send-keys -t $current_session:$current_window.$sail_pane_index "sail up" C-m
-#         tmux send-keys -t $current_session:$current_window.$terminal_pane_index "code ." C-m
-#         sleep 3
-#         tmux send-keys -t $current_session:$current_window.$terminal_pane_index "clear" C-m
-#     else
-#         echo "Project is not configured yet"
-#     fi
-# }
