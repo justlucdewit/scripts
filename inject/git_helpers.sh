6e09de3235696f3f79438cba41f9d81f9163f6cb
Luc de Wit <luc.dewit@hotmail.nl>
2024-10-29 15:14:21 +0100

    gitusers command for git user registrations in localsettings

--- inject/git_helpers.sh
+++ inject/git_helpers.sh
@@ -1,4 +1,133 @@
 # Define a function to check if you're in a Git repo and show the current branch
+alias gitusers="git_users_main_command"
+
+git_users_main_command() {
+    # Help command
+    if [ ! "$#" -gt 0 ]; then
+        echo "usage: "
+        echo "  - gitusers list"
+        echo "  - gitusers get <identifier>"
+        echo "  - gitusers new <identifier> <fullname>"
+        echo "  - gitusers del <identifier>"
+        echo "  - gitusers alias <identifier> <alias>"
+        echo "  - gitusers unlias <identifier> <alias>"
+        return 0
+    fi
+
+    local command=$1
+    shift
+
+    if is_in_list "$command" "list,all"; then
+        list_git_users $@
+    elif is_in_list "$command" "get"; then
+        get_git_user $@
+    elif is_in_list "$command" "new,create,add"; then
+        new_git_user $@
+    elif is_in_list "$command" "del,delete,rem,remove"; then
+        delete_git_user $@
+    elif is_in_list "$command" "add-alias,new-alias,create-alias,alias,"; then
+        set_git_user_alias $@
+    elif is_in_list "$command" "del-alias,rem-alias,delete-alias,remove-alias,unalias"; then
+        unset_git_user_alias $@
+    else
+        print_error "Command $command does not exist"
+        git_users_main_command # Re-run for help command
+    fi
+}
+
+set_git_user_alias() {
+    local identifier=$(prompt_if_not_exists "Identifier" $1)
+
+    # Attempt get, if already exists, error
+    local getResult=$(localsettings_eval ".gitusers.\"$identifier\"")
+    if [[ "$getResult" == "null" ]]; then
+        print_error "Git user with identifier $identifier does not exist"
+        return 1
+    fi
+
+    local alias=$(prompt_if_not_exists "Alias" $2)
+
+    localsettings_eval_with_save ".gitusers.\"$identifier\".aliases += [ \"$alias\" ]"
+
+    print_success "Added alias '$alias' to gituser '$identifier'"
+
+    localsettings_reformat
+}
+
+unset_git_user_alias() {
+    local identifier=$(prompt_if_not_exists "Identifier" $1)
+
+    # Attempt get, if already exists, error
+    local getResult=$(localsettings_eval ".gitusers.\"$identifier\"")
+    if [[ "$getResult" == "null" ]]; then
+        print_error "Git user with identifier $identifier does not exist"
+        return 1
+    fi
+
+    local alias=$(prompt_if_not_exists "Alias" $2)
+
+    localsettings_eval_with_save "del(.gitusers.\"$identifier\".aliases[] | select(. == \"$alias\"))"
+
+    print_success "Deleted alias '$alias' to gituser '$identifier'"
+
+    localsettings_reformat
+}
+
+get_git_user() {
+    # Get the needed values
+    local identifier=$(prompt_if_not_exists "Identifier" $1)
+    
+    localsettings_reformat
+
+    # Attempt get, if already exists, error
+    local getResult=$(localsettings_eval ".gitusers.\"$identifier\"")
+    if [[ "$getResult" == "null" ]]; then
+        print_error "Git user with identifier $identifier does not exist"
+        return 1
+    fi
+
+    localsettings_eval ".gitusers.\"$identifier\""
+}
+
+list_git_users() {
+    localsettings_reformat
+    localsettings_get .gitusers
+}
+
+delete_git_user() {
+    # Get the needed values
+    local identifier=$(prompt_if_not_exists "Identifier" $1)
+
+    # Attempt get, if not exists, error
+    local getResult=$(localsettings_eval ".gitusers.\"$identifier\"")
+    if [[ "$getResult" == "null" ]]; then
+        print_error "Git user with identifier $identifier does not exists"
+        return 1
+    fi
+
+    localsettings_delete ".gitusers.\"$identifier\""
+    print_success "Deleted gituser \"$identifier\""
+}
+
+new_git_user() {
+    local identifier=$(prompt_if_not_exists "Identifier" $1)
+
+    # Attempt get, if already exists, error
+    local getResult=$(localsettings_eval ".gitusers.\\\"$identifier\\\"")
+    if [[ ! "$getResult" == "null" ]]; then
+        print_error "Git user with identifier $identifier already exists"
+        return 1
+    fi
+
+    local fullname=$(prompt_if_not_exists "Fullname" $2)
+
+    # Set the values to the local settings
+    localsettings_eval_with_save ".gitusers.\"$identifier\".fullname = \"$fullname\"" > /dev/null
+    localsettings_eval_with_save ".gitusers.\"$identifier\".aliases = [ \"$fullname\" ]" > /dev/null
+    localsettings_reformat
+    print_success "Created gituser \"$identifier\""
+}
+
 parse_git_branch() {
     local branch
     branch=$(git branch --show-current 2>/dev/null)
