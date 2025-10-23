7f7f98ffdebf93f04eef7bda305f16fdd5128235
Luc de Wit <lucdewit@Lucs-MacBook-Pro.local>
2025-10-23 16:00:44 +0200

    Fully implemented all the old features of project cli

--- project/project.sh
+++ project/project.sh
@@ -1,6 +1,6 @@
 source "$HOME/.lsr_core/core/lsr.core.sh"
 
-# [ -s "$HOME/.nvm/nvm.sh" ] && \. "$HOME/.nvm/nvm.sh"
+[ -s "$HOME/.nvm/nvm.sh" ] && \. "$HOME/.nvm/nvm.sh"
 
 PROJECT_FILE="$HOME/projects/projects.json"
 PROJECT_DIR="$HOME/projects/"
@@ -58,6 +58,166 @@ project_go() {
     fi
 
     cd "$HOME/projects/$project_dir"
+}
+
+LSR_COMMAND_SET_HELP "project.prepare" "Sets correct node and php versions for this project"
+project_prepare() {
+    local project_json="$(project_json)"
+    local project_node_version="$(echo "$project_json" | jq -r ".versions.node")"
+    local project_php_version="$(echo "$project_json" | jq -r ".versions.php")"
+
+    if [[ "$project_node_version" != "null" ]]; then
+        print_info "Project uses node $project_node_version"
+    fi
+
+    if [[ "$project_php_version" != "null" ]]; then
+        print_info "Project uses php $project_php_version"
+    fi
+
+    # Set correct node version
+    if [[ "$project_node_version" != "null" ]]; then
+        nvm use "$project_node_version"
+    fi
+
+    # Set correct php version
+    if [[ "$project_php_version" != "null" ]]; then
+        brew unlink php
+        brew link "php@$project_php_version"
+    fi
+}
+
+LSR_COMMAND_SET_HELP "project.install" "Completely installs the project from scratch"
+project_install() {
+    local repo_prefix="git@bitbucket.org:xingredient/"
+    local env_folder="$HOME/projects/env_files"
+
+    if [[ "$(ls)" != "" ]]; then
+        print_error "Can only install project from scratch, but this folder contains files"
+        return
+    fi
+
+    # Cloning repository
+    local project_json="$(project_json)"
+    local project_repo="$(echo "$project_json" | jq -r ".repo")"
+    local project_code="$(echo "$project_json" | jq -r ".code")"
+    local repo_url="$repo_prefix$project_repo.git"    
+    local post_clone_commands="$(echo "$project_json" | jq -r "(.post_clone_commands // [])[]")"
+    
+    print_info "cloning repository $repo_url"
+
+    git clone $repo_url . > /dev/null
+
+    # Place .env file
+    if [[ -f "$env_folder/$project_code.env" ]]; then
+        print_info "Copying over backup .env file"
+        cp "$env_folder/$project_code.env" .env
+    else
+        print_warn "not able to find $project_code.env file backup"
+    fi
+
+    echo "$post_clone_commands" | while IFS= read -r command_to_run; do
+        print_info "post clone command: $command_to_run"
+        
+        # Execute the command string
+        eval "$command_to_run"
+        
+        EXIT_CODE=$?
+    done
+
+    # Make sure we use the correct node and composer versions
+    project_prepare
+
+    # Install node packages
+    if [[ -f "./package.json" ]]; then
+        if [[ ! -d "./node_modules" ]]; then
+            print_info "Installing node packages..."
+            npm i --silent > /dev/null
+        fi
+    fi
+
+    # Install vendor packages
+    if [[ -f "./composer.json" ]]; then
+        if [[ ! -d "./vendor" ]]; then
+            print_info "Installing composer packages..."
+            composer i -n > /dev/null
+        fi
+    fi
+
+    print_info "All packages installed!"
+
+    # If sail is installed, create the sail containers and run migrations, and link storage
+    if [[ -f "./vendor/bin/sail" ]]; then
+        ./vendor/bin/sail create
+        ./vendor/bin/sail start
+        ./vendor/bin/sail artisan storage:link
+        sleep 5 # Sleep to make sure the migration can succesfully happen and is not too early
+        ./vendor/bin/sail artisan migrate
+        
+        # Create filament user if filament is installed
+        local filament_package="$(cat composer.json | grep filament/filament)"
+        if [[ $filament_package != "" ]]; then
+            print_info "Creating filament user"
+            ./vendor/bin/sail artisan make:filament-user --name=admin --email=admin@x-ingredient.nl --password="password"
+        fi
+    fi
+}
+
+LSR_COMMAND_SET_HELP "project.remove" "Deinstall the project by removing the project files & docker containers"
+project_remove() {
+
+    # If project contains sail, we must make sure the container is removed first
+    if [[ -f ./vendor/bin/sail ]]; then
+
+        # If docker deamon is not running, start it
+        print_info "checking docker containers..."
+        docker info > /dev/null 2>&1 || {
+            print_info "docker deamon not running, attempting to start..."
+
+            open -a Docker --args --user-initiated > /dev/null 2>&1
+
+            i=0
+            while ! docker info > /dev/null 2>&1; do
+                # Add a visual wait indicator
+                printf "."
+                sleep 1
+                i=$((i+1))
+                # Optional: Add a timeout to prevent an infinite loop
+                if [ $i -ge 60 ]; then
+                    print_error "docker daemon failed to start after 60 seconds."
+                    return
+                fi
+            done
+            echo "" # Print newline due to docker startup printing
+        }
+
+        # If container exists, remove it
+        local container_ids="$(./vendor/bin/sail ps -aq)"
+        if [[ "$container_ids" != "" ]]; then
+            print_info "removing docker containers..."
+            ./vendor/bin/sail down -v > /dev/null
+        else
+            print_info "docker containers were already removed"
+        fi
+    fi
+    
+    # Removing all project files
+    local rm_executable="$(which rm)"
+    print_info "removing project files..."
+    $rm_executable -rf *
+    $rm_executable -rf .??*
+
+    print_success "project deinstalled"
+}
+
+LSR_COMMAND_SET_HELP "project.open-repo" "Open repository of this project"
+project_open_repo() {
+    local default_repo_prefix="https://bitbucket.org/xingredient"
+
+    local project_json="$(project_json)"
+    local repo_name="$(echo "$project_json" | jq -r ".repo")"
+    local repo_url="$default_repo_prefix/$repo_name"
+
+    open "$repo_url"
 }
 
 LSR_COMMAND_SET_HELP "project.open-site" "Open a website of the current project"
@@ -229,363 +389,3 @@ project_open_forge() {
     echo "Opening $forge_base_url$url_postfix..."
     open "$forge_base_url$url_postfix"
 }
-
-# LSR_COMMAND "project" "$@"
-
-# LSR_SET_SUBCOMMAND "go" "<projectname>"
-# LSR_SET_SUBCOMMAND "install" "<projectname>"
-# LSR_SET_SUBCOMMAND "uninstall" "<projectname>"
-# LSR_SET_SUBCOMMAND "start" "<projectname>"
-# LSR_SET_SUBCOMMAND "build" "<projectname>"
-
-# LSR_SET_SUBCOMMAND "info" "<projectname>"
-
-# LSR_DESCRIBE_SUBCOMMAND "list" "List all of the registered projects and demos"
-# LSR_DESCRIBE_SUBCOMMAND "go" "Go to the directory of the project"
-# LSR_DESCRIBE_SUBCOMMAND "install" "Install the project locally"
-# LSR_DESCRIBE_SUBCOMMAND "uninstall" "Install the project locally"
-# LSR_DESCRIBE_SUBCOMMAND "start" "Starts the project in the current terminal"
-# LSR_DESCRIBE_SUBCOMMAND "build" "Makes a build of the project"
-# LSR_DESCRIBE_SUBCOMMAND "current" "Shows the current project code"
-# LSR_DESCRIBE_SUBCOMMAND "links" "List all websites or resources belonging to this project"
-# LSR_DESCRIBE_SUBCOMMAND "info" "Show info about project"
-
-# LSR_CLI_INPUT_PARSER $@
-
-# DEFAULT_CONFIG_TO_CURRENT="true"
-
-# project_repo_to_name() {
-#     local repo="$1"
-
-#     # Make sure git repository is cloned
-#     if [[ "$repo" == "codecommit::"* ]]; then
-#         # Codecommit repository
-#         repo=$(echo "$repo" | sed -E 's|.*//([^/]+)|\1|')
-#     fi
-
-#     echo "$repo"
-# }
-
-# project_query_to_code() {
-#     local query="$1"
-
-#     if str_empty "$query" && str_equals "$DEFAULT_CONFIG_TO_CURRENT" "true"; then
-#         query="$(project_current)"
-#     fi
-
-#     local project_code1="$(jq -r ".[] | select(.code == \"$query\") | .code" "$HOME/projects/projects.json")"
-#     local project_code2="$(jq -r ".[] | select(.code == \"website-$query\") | .code" "$HOME/projects/projects.json")"
-#     local project_code3="$(jq -r ".[] | select(.code == \"demo-$query\") | .code" "$HOME/projects/projects.json")"
-#     local project_code4="$(jq -r ".[] | select(.code == \"demo-\" + .client + \"-$query\") | .code" "$HOME/projects/projects.json")"
-#     local project_code5="$(jq -r ".[] | select(.code == \"website-\" + .client + \"-$query\") | .code" "$HOME/projects/projects.json")"
-
-#     if ! str_empty "$project_code1"; then
-#         echo "$project_code1"
-#         return
-#     fi
-
-#     if ! str_empty "$project_code2"; then
-#         echo "$project_code2"
-#         return
-#     fi
-
-#     if ! str_empty "$project_code3"; then
-#         echo "$project_code3"
-#         return
-#     fi
-
-#     if ! str_empty "$project_code4"; then
-#         echo "$project_code4"
-#         return
-#     fi
-
-#     if ! str_empty "$project_code5"; then
-#         echo "$project_code5"
-#         return
-#     fi
-# }
-
-# project_helper_find_config() {
-#     local query="$1"
-#     local project_code="$(project_query_to_code "$query")"
-
-#     if str_empty "$query" && str_equals "$DEFAULT_CONFIG_TO_CURRENT" "true"; then
-#         query="$(project_current)"
-#     fi
-
-#     local project_json="$(jq -r ".[] | select(.code == \"$project_code\")" "$HOME/projects/projects.json")"
-#     echo "$project_json"
-# }
-
-# project_code_to_query() {
-#     local code="$1"
-#     local code="$(project_query_to_code "$code")"
-#     local json="$(project_helper_find_config "$code")"
-#     local client="$(echo "$json" | jq -r ".client")"
-
-#     code="$(echo "$code" | sed -E 's/^(website-|demo-)//')"
-#     if [[ "$client" != "null" ]]; then
-#         code="$(echo "$code" | sed -E "s/^($client-)//")"
-#     fi
-
-#     echo "$code"
-# }
-
-# project_info() {
-#     local project_json="$(project_helper_find_config "$query")"
-
-#     local project_active="$(echo "$project_json" | jq -r ".active")"
-#     local project_repo="$(echo "$project_json" | jq -r ".repo")"
-#     local project_dir="$(echo "$project_json" | jq -r ".dir")"
-#     local project_code="$(echo "$project_json" | jq -r ".code")"
-#     local project_websites="$(echo "$project_json" | jq -r "(.sites // []) | to_entries[] | .key")"
-#     local project_servers="$(echo "$project_json" | jq -r "(.forgeIds // []) | to_entries[] | .key")"
-
-#     echo
-#     echo "Project Info for '$project_code'"
-#     if str_equals "$project_active" "true"; then
-#         echo "  - Project is active"
-#     else
-#         echo "  - Project is inactive"
-#     fi
-
-#     echo "  - Repository: https://bitbucket.org/xingredient/$project_repo/src"
-
-#     echo
-#     echo "Technology used: "
-#     local tech_used="$(echo "$project_json" | jq -r ".versions | to_entries[] | .key")"
-#     for tech in $(echo "$tech_used" | tr '\n' ' '); do
-#         local version="$(echo "$project_json" | jq -r ".versions.$tech")"
-#         echo -e "  - $LSR_COLOR_GREEN$tech:$LSR_COLOR_RESET v$version"
-#     done
-
-#     echo
-#     local branches_list="$(git for-each-ref --format='%(refname:short) %(authordate:short) %(authorname)' refs/heads)"
-#     local normal_count=0
-#     local feature_count=0
-#     local text_buffer=""
-#     while read -r branch date author; do
-#         if [[ "$branch" == feature/* ]]; then
-#             ((feature_count++))
-#         else
-#             ((normal_count++))
-#         fi
-        
-#         if [[ -n text_buffer ]]; then
-#             text_buffer+=$'\n'
-#         fi
-#         text_buffer="$text_buffer - $branch (last updated on $LSR_COLOR_GREEN$date$LSR_COLOR_RESET by $LSR_COLOR_GREEN$author$LSR_COLOR_RESET)"
-#     done <<< "$branches_list"
-#     echo "Branches ($normal_count normal branches; $feature_count feature branches):"
-#     echo -e "$text_buffer"
-
-#     echo
-# }
-
-# # uninstallation of node packages
-# # uninstallation of composer packages
-# project_uninstall() {
-#     local project_entry="$(project_helper_find_config "$1")"
-
-#     if str_empty "$project_entry"; then
-#         print_error "No project found with code '$1'"
-#         return
-#     fi
-
-#     local project_repo="$(echo "$project_entry" | jq -r ".dir")"
-
-#     # Uninstall node packages
-#     if [[ -d "$HOME/projects/$project_repo/node_modules" ]]; then
-#         print_success "Uninstalled node packages"
-#         rm -rf "$HOME/projects/$project_repo/node_modules"
-#     fi
-
-#     #Uninstall composer packages
-#     if [[ -d "$HOME/projects/$project_repo/vendor" ]]; then
-#         print_success "Uninstalled composer"
-#         rm -rf "$HOME/projects/$project_repo/vendor"
-#     fi
-# }
-
-# # installation of repository
-# # installation of node version
-# # installation of php version
-# # installation of node packages
-# # installation of composer packages
-# project_install() {
-#     local project_entry="$(project_helper_find_config "$1")"
-
-#     if str_empty "$project_entry"; then
-#         print_error "No project found with code '$1'"
-#         return
-#     fi
-
-#     local project_code="$(echo "$project_entry" | jq -r ".code")"
-#     local project_repo="$(echo "$project_entry" | jq -r ".repo")"
-#     local project_dir="$(echo "$project_entry" | jq -r ".dir")"
-#     local node_version="$(echo "$project_entry" | jq -r ".versions.node // \"\"")"
-#     local php_version="$(echo "$project_entry" | jq -r ".versions.php // \"\"")"
-
-#     # Make sure git repository is cloned
-#     if [[ "$project_repo" == "codecommit::"* ]]; then
-
-#         # Default URL repo
-#         if [[ ! -d "$HOME/projects/$project_dir" ]]; then
-#             print_info "Cloning repository '$project_dir'..."
-#             git clone $project_repo "$HOME/projects/$project_dir"
-#         else
-#             print_success "Repository '$project_dir' already cloned!"
-#         fi
-#     else
-#         # Default URL repo
-#         if [[ ! -d "$HOME/projects/$project_dir" ]]; then
-#             print_info "Cloning repository '$project_dir'..."
-
-#             git clone git@bitbucket.org:xingredient/$project_repo.git "$HOME/projects/$project_dir"
-#         else
-#             print_success "Repository '$project_dir' already cloned!"
-#         fi
-#     fi
-
-
-#     # Make sure node version is installed
-#     if [[ -n "$node_version" ]]; then
-#         if [[ "$(nvm ls "$node_version")" == *"N/A"* ]]; then
-#             print_info "Installing node v$node_version..."
-#             nvm i "$node_version"
-#         else
-#             print_success "Node v$node_version already installed!"
-#         fi
-
-#         nvm use "$node_version" > /dev/null
-#     fi
-
-#     # Make sure php version is installed
-#     if [[ -n "$php_version" ]]; then
-#         brew install "php@$php_version"
-#         brew link "php@$php_version"
-#         # sudo update-alternatives --set php /usr/bin/php$php_version > /dev/null
-#     fi
-
-#     # Make sure npm packages are installed when available
-#     if [[ -f "$HOME/projects/$project_dir/package.json" ]]; then
-#         if [[ ! -d "$HOME/projects/$project_dir/node_modules" ]]; then
-#             print_info "Installing node packages..."
-#             cd "$HOME/projects/$project_dir"
-#             npm install
-#         else
-#             print_success "Node packages already installed..."
-#         fi
-#     fi
-
-#     # Make sure composer packages are installed when available
-#     if [[ -f "$HOME/projects/$project_dir/composer.json" ]]; then
-#         if [[ ! -d "$HOME/projects/$project_dir/vendor" ]]; then
-#             print_info "Installing composer packages..."
-#             cd "$HOME/projects/$project_dir"
-#             composer install
-#         else
-#             print_success "Composer packages already installed..."
-#         fi
-#     fi
-# }
-
-# project_start() {
-#     local project_entry="$(project_helper_find_config "$1")"
-
-#     if str_empty "$project_entry"; then
-#         print_error "No project found with code '$1'"
-#         return
-#     fi
-
-#     local project_code="$(echo "$project_entry" | jq -r ".code")"
-#     local project_repo="$(echo "$project_entry" | jq -r ".repo")"
-#     local project_dir="$(echo "$project_entry" | jq -r ".dir")"
-#     local node_version="$(echo "$project_entry" | jq -r ".versions.node // \"\"")"
-#     local php_version="$(echo "$project_entry" | jq -r ".versions.php // \"\"")"
-#     local on_start_commands="$(echo "$project_entry" | jq -r '.config.on_start // empty | .[]')"
-
-#     # # Use the correct php and npm versions if defined
-#     # if [[ -n "$php_version" ]]; then
-#     #     # sudo update-alternatives --set php /usr/bin/php$php_version > /dev/null
-#     #     echo
-#     # fi
-
-#     # if [[ -n "$node_version" ]]; then
-#     #     nvm use $node_version
-#     # fi
-
-#     # Change the directory to the project
-#     current_pane=$(tmux display-message -p '#{pane_id}')
-#     tmux send-keys -t "$current_pane" "cd $HOME/projects/$project_dir" C-m
-
-#     # Execute the on_start commands
-#     while IFS= read -r command; do
-#         tmux send-keys -t "$current_pane" "$command" C-m    
-#     done <<< "$on_start_commands"
-# }
-
-# project_build() {
-#     local project_entry="$(project_helper_find_config "$1")"
-
-#     if str_empty "$project_entry"; then
-#         print_error "No project found with code '$1'"
-#         return
-#     fi
-
-#     local project_code="$(echo "$project_entry" | jq -r ".code")"
-#     local project_repo="$(echo "$project_entry" | jq -r ".repo")"
-#     local project_dir="$(echo "$project_entry" | jq -r ".dir")"
-#     local node_version="$(echo "$project_entry" | jq -r ".versions.node // \"\"")"
-#     local php_version="$(echo "$project_entry" | jq -r ".versions.php // \"\"")"
-#     local on_build_commands="$(echo "$project_entry" | jq -r '.config.on_build // empty | .[]')"
-
-#     # Use the correct php and npm versions if defined
-#     if [[ -n "$php_version" ]]; then
-#         # sudo update-alternatives --set php /usr/bin/php$php_version > /dev/null
-#         echo
-#     fi
-
-#     if [[ -n "$node_version" ]]; then
-#         nvm use $node_version
-#     fi
-
-#     # Change the directory to the project
-#     current_pane=$(tmux display-message -p '#{pane_id}')
-#     tmux send-keys -t "$current_pane" "cd $HOME/projects/$project_dir" C-m
-
-#     # Execute the on_start commands
-#     while IFS= read -r command; do
-#         tmux send-keys -t "$current_pane" "$command" C-m    
-#     done <<< "$on_build_commands"
-# }
-
-# project_go() {
-#     local query="$1"
-
-#     # If no query, list projects
-#     if [[ -z "$query" ]]; then
-#         project_list
-#         return 0
-#     fi
-
-#     local entry="$(project_helper_find_config "$query")"
-
-#     local project_repo="$HOME/projects/$(echo "$entry" | jq -r ".repo")"
-#     local project_dir="$HOME/projects/$(echo "$entry" | jq -r ".dir")"
-#     project_repo="$(project_repo_to_name "$project_repo")"
-    
-#     # Check if the provided project exists
-#     if ! str_empty "$entry"; then
-#         if [[ -d "$project_dir" ]]; then
-#             current_pane=$(tmux display-message -p '#{pane_id}')
-#             tmux send-keys -t "$current_pane" "cd $project_dir" C-m
-#         else
-#             echo "Directory does not exist: $project_dir"
-#         fi
-#         return
-#     fi
-    
-#     echo "Project '$query' not found. Available projects:"
-#     project_list
-# }
