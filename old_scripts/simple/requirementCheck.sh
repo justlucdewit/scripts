82649671faa944dc23556d7a636a084955b58072
Luc de Wit <lucdewit@Lucs-MacBook-Pro.local>
2025-10-23 09:45:40 +0200

    Removed all files from cache except some scripts

--- old_scripts/simple/requirementCheck.sh
+++
@@ -1,7 +0,0 @@
-# This file includes require package statements that makes sure that all packages needed for a basic LSR
-# installation are indeed correctly installed and callable
-requires_package "yq" "LSR"
-requires_package "jq" "LSR"
-requires_package "node" "LSR"
-requires_package "npm" "LSR"
-requires_package "git" "LSR"
